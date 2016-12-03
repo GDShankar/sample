eaace6d4a14d979f959ffa581a47d8e9e481caf7
Elbissop1 <Elbissop1>
2016-12-03 12:39:50 +0530

    Initial commit

---
+++ app/src/main/java/com/elbissopsoft/bill/foodorder.java
@@ -0,0 +1,13 @@
+package com.elbissopsoft.bill;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class foodorder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodorder);
+    }
+}
